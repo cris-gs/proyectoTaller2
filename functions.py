33f118a8edd8928a0ca227c6c0a5591d76ee3ddd
cris-gs <61550370+cris-gs@users.noreply.github.com>
2020-07-03 15:40:34 -0600

    tercera carga

--- functions.py
+++ functions.py
@@ -3,20 +3,34 @@ from tkinter import filedialog
 from PIL import ImageTk
 from PIL import Image
 from visionAPI import reconocer_caras
+from clases import *
 root = Tk()
+
 root.title('canvas')
 
 root.filename =  filedialog.askopenfile(initialdir = "/",title = "Select file",filetypes = (("jpeg files","*.jpg"),("all files","*.*")))
+#"""[obtiene los datos de la imagen que obtiene roo.filename y lo guarda en un archivo.txt
+#para separa la ubicación de la imagen de los demas datos]
+#"""
 a=str(root.filename)
 b=a.find("mode")
-textfile=open("archivo1.txt","tw")
+textfile=open("ubicacion.txt","tw")
 textfile.write(a)
 textfile.seek(23)
 textfile.write("\n")
 textfile.seek(b-2)
 textfile.write("\n")
 textfile.close()
-def leeLineas(n,archivo):
+def lee_Lineas(n,archivo):
+    """[funcion que recorre linea por linea un archivo.txt]
+
+    Args:
+        n ([type:int]): [este es un puntero que se debe digitar con la cantidad de lineas que tiene el archivo.txt]
+        archivo ([type:str]): [obtiene el nombre del archivo digitado al llamar la función para que se abra el forma de lectura]
+        cacaracter([type:str-bytes]):[se le asignan los datos de la lectura de cada linea]
+
+        return(respuesta):([type:list]):[retorna una lista con los datos de cada linea del archivo.txt]
+    """
     file=open(archivo,"tr")
     respuesta=list()
     while n>1:
@@ -37,11 +51,37 @@ def leeLineas(n,archivo):
     file.close()   
     return(respuesta)
 
-l=str(leeLineas(3,"archivo1.txt")[1])
+"""[l es un string que obtiene la ubición de la imagen que se obtiene llamando la función de leeLineas]
+"""
+l=str(lee_Lineas(3,"archivo1.txt")[1])
+   
+def imprimir():
+    """[Función que crea un archivo con los el nombre de la persona digitada en el Entry de etiquetar
+    esta función es llamada al seleccionar el boton guardar]
+    """
+    dato=Nombre.get()
+    #print(type(dato))
+    textfile=open("dato.txt","tw")
+    textfile.write(dato)
+    textfile.close()
+
+    textfile=open("dato.txt","a")
+    textfile.write(".bin")
+    textfile.close()
+
+    textfile=open("dato.txt", "tr")
+    registro=textfile.read()
+    textfile.close()
+
+    file=open(registro, "bw")
+    guardarArchivo(ListaPersonas,file)
+    file.close()
+    
 
 
-#print (root.filename.name)
 
+"""[permite que la ventana para mostar a imagen en la interfaz grafica tenga la opción de utilizar el scroll del mouse]
+"""
 scrollbary = Scrollbar(root)
 scrollbary.pack( side = RIGHT, fill = Y )
 
@@ -50,19 +90,66 @@ scrollbarx.pack( side = BOTTOM, fill = X )
 
 
 # circulo
+"""[Abre una ventana de interfaz grafica]
+"""
 canvas = Canvas(width=500, height=500, bg='white', xscrollcommand = scrollbarx.set, yscrollcommand = scrollbary.set)
 canvas.pack(expand=YES, fill=BOTH)
 
 scrollbary.config( command = canvas.yview )
 scrollbarx.config( command = canvas.xview )
 
-##img = PhotoImage(file="flor.jpg")      
+"""[crea una ventana de interfaz grafica adiccional para mostar las opciones de etiquetar a la persona de la imagen
+o realizar una busqueda por nombre de otras imagenes ya etiquetadas]
+"""
+ventana=Tk()
+ventana.title("Etiquetar-Buscar")
+ventana.geometry('310x150')
+
+lbl = Label(ventana, text="--------Etiquetar--------")
+lbl.grid(column=1, row=0,padx=(20,5))
+
+lbl = Label(ventana, text="Nombre")
+lbl.grid(column=0, row=2,padx=(20,5))
+Nombre = Entry(ventana,width=20)
+Nombre.grid(column=1, row=2,padx=(0,10))
+
+btn = Button(ventana, text="Guardar",command=imprimir)
+btn.grid(column=2, row=2,padx=(10,10))
+
+
+lbl = Label(ventana, text="--------Buscar--------")
+lbl.grid(column=1, row=6,padx=(20,5))
+
+lbl = Label(ventana, text="Nombre")
+lbl.grid(column=0, row=8,padx=(20,5))
+txt = Entry(ventana,width=20)
+txt.grid(column=1, row=8,padx=(0,10))
+
+btn = Button(ventana, text="Buscar")
+btn.grid(column=2, row=8,padx=(10,10))
+
+
+
+#lb_nombre=tk.Label(canvas,text="Nombre: ")
+#lb_apellido1=tk.Label(canvas,text="Primer Apellido: ")
+#lb_apellido2=tk.Label(canvas,text="Segundo Apellido: ")
+
+
+##img = PhotoImage(file="flor.jpg") 
+  
 img = ImageTk.PhotoImage(Image.open(root.filename.name))
-canvas.create_image(0,0, anchor=NW, image=img)      
+canvas.create_image(0,0, anchor=NW, image=img)
+
+"""[llama al metodo reconocer_caras desde visionAPI.py para realizar el reconocimiento 
+del rostro mostrado en la imagen con la ubicación anteriormente asignada a "l"]
+"""
 r=reconocer_caras(l)
 print(r)
 cont=0
 d=len(r)
+
+"""[realiza un ciclo para crear un recuadro sobre el o los rostros reconocidos en la imagen seleccinada]
+"""
 while cont<d:
     x1=(r[cont]["vertices"][0]["x"])
     y1=(r[cont]["vertices"][0]["y"])
@@ -72,4 +159,5 @@ while cont<d:
     cont+=1
 
 root.mainloop()
+#ventana.mainloop()
 
